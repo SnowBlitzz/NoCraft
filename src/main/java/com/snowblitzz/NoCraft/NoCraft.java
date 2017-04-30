d2d6eed76933ec61cac460459e291e43086c414e
SnowBlitzz <784559099@qq.com>
2017-04-30 21:01:57 +0800

    Add files via upload

---
+++ src/main/java/com/snowblitzz/NoCraft/NoCraft.java
@@ -0,0 +1,30 @@
+package com.snowblitzz.NoCraft;
+
+import org.spongepowered.api.block.BlockTypes;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.block.InteractBlockEvent;
+import org.spongepowered.api.event.filter.cause.First;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
+
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
+
+@Plugin(id = "nocraft", name = "NoCraft", version = "1.0.0", description = "Disallows right clicking on workbench.")
+public class NoCraft {	
+	
+	@Listener
+	public void onRightClick(InteractBlockEvent.Secondary.MainHand event, @First Player player) {
+		
+		if(event.getTargetBlock().getLocation().isPresent()) {
+			Location<World> block = event.getTargetBlock().getLocation().get();
+	        if(block.getBlock().getType() == BlockTypes.CRAFTING_TABLE) {
+	        	event.setCancelled(true);
+	        	((EntityPlayerMP) player).addChatMessage(new TextComponentString(TextFormatting.RED + "You are not allowed to craft with the workbench."));
+	        }
+		}
+	}
+}
